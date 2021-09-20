26a28a6aec29e053f7a40b58694eff9346750b49
Michal Prihoda <michal@prihoda.net>
2021-09-20 13:18:46 +0200

    Bump up ZIO to 1.0.11.

--- sbt-iw-projects/src/main/scala/works/iterative/sbt/IWMaterialsPlugin.scala
+++ sbt-iw-projects/src/main/scala/works/iterative/sbt/IWMaterialsPlugin.scala
@@ -19,7 +19,7 @@ object IWMaterialsVersions {
   val play = "2.8.8"
   val playJson = "2.9.2"
   val scalaTest = "3.2.9"
-  val zio = "1.0.9"
+  val zio = "1.0.11"
   val zioConfig = "1.0.6"
   val zioJson = "0.1.5"
   val zioLogging = "0.5.11"
