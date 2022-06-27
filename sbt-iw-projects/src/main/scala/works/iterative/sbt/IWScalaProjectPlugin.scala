a78c67a4c1d19bd8ce39cfa1e6294c2a649fe429
Michal Prihoda <michal@prihoda.net>
2022-06-27 22:47:35 +0200

    Scala and sbt-native-packager version bump.

--- sbt-iw-projects/src/main/scala/works/iterative/sbt/IWScalaProjectPlugin.scala
+++ sbt-iw-projects/src/main/scala/works/iterative/sbt/IWScalaProjectPlugin.scala
@@ -15,7 +15,7 @@ object IWScalaProjectPlugin extends AutoPlugin {
 
   object autoImport {
     val scala2Version = "2.13.8"
-    val scala3Version = "3.1.2"
+    val scala3Version = "3.1.3"
     def publishToIW: Seq[Def.Setting[_]] = inThisBuild(
       List(
         publishTo := {
